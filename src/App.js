c3c1b09bc91f42a302161834521e53a5744a47a4
Juĺia Sperandio <juhsperandio@hotmail.com>
2020-11-25 13:23:03 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import { Card, Col, Row } from "antd";
 
 function App() {
+  const [user, setUser] = useState({ user: {}, active: false });
+
+  const handleToggle = () => {
+    console.log("handleToggle");
+
+    fetch("https://api.github.com/users/CodeWithHarry")
+      .then((res) => res.json())
+      .then((data) => setUser({ user: data, active: !user.active }));
+  };
+
+  console.log(user.active);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user.active === true && (
+        <div>
+          <div className="site-card-wrapper">
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card title="Avatar" bordered={false}>
+                  <img src={user.user.avatar_url} />
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="Login" bordered={false}>
+                  {user.user.login}
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="ID" bordered={false}>
+                  {user.user.id}
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="E-mail" bordered={false}>
+                  {user.user.email}
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      )}
+
+      <button onClick={() => handleToggle()}> Mostrar/Esconder </button>
     </div>
   );
 }
